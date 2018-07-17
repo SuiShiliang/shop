7ada5d88d1830206dcae14bfdf9491ceab3feaba
Sui Shiliang <1075670749@qq.com>
2018-07-17 10:34:40 +0800

    在配置文件appconfig中配置mybatis相关（datasource、sqlsessionfactorybean）

--- src/main/java/shop/config/AppConfig.java
+++ src/main/java/shop/config/AppConfig.java
@@ -1,7 +1,14 @@
 package shop.config;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,5 +21,24 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/jsp",".jsp");
+	}
+	@Bean
+	public DataSource dataSource(Environment env) {
+		String jdbcurl = env.getProperty("");
+		String username = env.getProperty("");
+		String password = env.getProperty("");
+		String driverClassName = env.getProperty("");
+		DriverManagerDataSource ds = 
+				new DriverManagerDataSource(jdbcurl,username,password);
+		ds.setDriverClassName(driverClassName);
+		return ds;
+	}
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource ds) {
+		SqlSessionFactoryBean sf = new SqlSessionFactoryBean();
+		sf.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+		sf.setDataSource(ds);
+		return sf;
+
 	}
 }
